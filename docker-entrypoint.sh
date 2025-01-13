fe26ceba7f0f2ad58b5b7e3a494c614f40bac274
Brett <25758102+ngen-brett@users.noreply.github.com>
2025-01-12 21:03:45 -0500

    Updated boostrapping modifiable config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ "$user" = '0' ]; then
 	[ -d "/mosquitto" ] && chown -R mosquitto:mosquitto /mosquitto || true
 fi
 
-if [ -f "/mosquitto/config/mosquitto.conf" ]
+if [ ! -f "/mosquitto/config/mosquitto.conf" ]
 then
   cp /mosquitto/default/mosquitto-no-auth.conf /mosquitto/config/mosquitto.conf
 fi
