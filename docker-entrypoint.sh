fa0787b923bc109dbbf028002b963675043ea589
Brett <25758102+ngen-brett@users.noreply.github.com>
2025-01-12 21:06:18 -0500

    Updated boostrapping modifiable config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,11 +4,13 @@ set -e
 # Set permissions
 user="$(id -u)"
 if [ "$user" = '0' ]; then
+  echo "Updating ownership for /mosquitto to mosquitto:mosquitto" 
 	[ -d "/mosquitto" ] && chown -R mosquitto:mosquitto /mosquitto || true
 fi
 
 if [ ! -f "/mosquitto/config/mosquitto.conf" ]
 then
+  echo "Populating minimal config - remote without authentication"
   cp /mosquitto/default/mosquitto-no-auth.conf /mosquitto/config/mosquitto.conf
 fi
 
